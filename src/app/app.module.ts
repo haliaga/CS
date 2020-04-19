f3bff66b2953eca4e69d328c2c7b8fb8502c0af8
DESKTOP-JORUA66\haliaga <horacio.aliaga@gmail.com>
2020-04-19 19:10:51 -0400

    on the way to use http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { CompaniesComponent } from './companies/companies.component';
 import { CsDetailComponent } from './cs-detail/cs-detail.component';
 import { MsgsComponent } from './msgs/msgs.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +21,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
