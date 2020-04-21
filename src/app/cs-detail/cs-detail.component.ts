27eb6fbb8f70158923536da70e2205e0515c849b
DESKTOP-JORUA66\haliaga <horacio.aliaga@gmail.com>
2020-04-20 20:32:05 -0400

    works like the original

--- src/app/cs-detail/cs-detail.component.ts
+++ src/app/cs-detail/cs-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { CsService } from '../cs.service';
@@ -11,7 +11,7 @@ import { Company } from '../Company';
 })
 
 export class CsDetailComponent implements OnInit {
-  company: Company;
+  @Input() company: Company;
   constructor(
     private route: ActivatedRoute,
     private csService: CsService,
